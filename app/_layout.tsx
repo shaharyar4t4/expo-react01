cdb55b231a7416648d17b71dec7c015d9cd6629a
Shaharyar <shaharyar4t4@gmail.com>
2025-07-16 01:27:10 +0500

    updated

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Button, Pressable, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 
 
@@ -6,10 +6,29 @@ export default function RootLayout() {
 
    return(
     <View style={style.container}>
-
      <View style={style.mainbox}>
-      <Text style={style.text}>Expo React Native Class 01</Text>
+      <Text style={style.subtext}>Expo React Native Class 01</Text>
      </View>
+     <View>
+      <Text style={style.tophead}>Hi there 👋🏻</Text>
+      <Text style={style.minibox}>I am Shaharyar Ali</Text>
+      <Text style={style.minibox}>BSCS</Text>
+     </View>
+    <View >
+      {/*this button is not multiple customise things */}
+      <Button title="Submit" onPress={()=>console.log("Shaharyar")} color={"blue"}/>
+              <TouchableOpacity style={style.btnstyle}>
+          <Text style={style.plaintxt}>Touchable Opacity</Text>
+        </TouchableOpacity>
+
+        <Pressable style={({pressed})=>[
+          style.btnstyle,
+          pressed && style.btnhovestyle
+        ]}>
+          <Text style={style.plaintxt}>Pressable</Text>
+        </Pressable>
+
+    </View>
     </View>
    );
 
@@ -20,17 +39,58 @@ const style = StyleSheet.create({
     flex: 1,
     backgroundColor: "#fff"
    },
+   tophead:{
+      fontWeight: "bold",
+    fontFamily: "Poppins",
+    fontSize: 30,
+    paddingTop: 10,
+    paddingLeft: 15,
+   },
    mainbox:{
-
-    backgroundColor: "#000",
+    backgroundColor: "#1D2671",
     width:"100%",
+    height: "20%",
     borderBottomRightRadius: 20,
     borderBottomLeftRadius: 20,
+    justifyContent: "center",
     padding:20,
    },
-   text:{
+   minibox:{
+      paddingTop: 15,
+      paddingLeft: 15,
+      borderColor: "#c4c4c4",
+      fontSize: 18,
+      borderBlockColor: "#c4c4c4",
+      borderWidth: 1,
+      margin: 10,
+      borderRadius:10,
+   },
+   subtext:{
     color:  "#fff",
-    fontSize: 20
+    fontSize: 20,
+    textAlign: "center",
+    fontWeight: "bold",
+    fontFamily: "Poppins"
+   },
+   btnstyle:{
+     margin:10,
+     padding: 10,
+     borderRadius: 10,
+     backgroundColor: "#000",
+     justifyContent: "center",
+     
+   },
+   btnhovestyle:{
+     margin:10,
+     padding: 10,
+     borderRadius: 10,
+     backgroundColor: "#c4c4c4",
+     justifyContent: "center",
+     
+   },
+   plaintxt:{
+         color:  "#fff",
+         justifyContent: "center",
+         alignContent: "center",
    }
-
 });
