313bd90c2002661ae3cd1eac6183a58f8f1bac7c
Shaharyar <shaharyar4t4@gmail.com>
2025-07-16 01:36:54 +0500

    udpdated

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,61 +1,70 @@
-import { Button, Pressable, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { Button, Pressable, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
 
 
 
 export default function RootLayout() {
 
-   return(
-    <View style={style.container}>
-     <View style={style.mainbox}>
-      <Text style={style.subtext}>Expo React Native Class 01</Text>
-     </View>
-     <View>
-      <Text style={style.tophead}>Hi there 👋🏻</Text>
-      <Text style={style.minibox}>I am Shaharyar Ali</Text>
-      <Text style={style.minibox}>BSCS</Text>
-     </View>
-    <View >
-      {/*this button is not multiple customise things */}
-      <Button title="Submit" onPress={()=>console.log("Shaharyar")} color={"blue"}/>
-              <TouchableOpacity style={style.btnstyle}>
-          <Text style={style.plaintxt}>Touchable Opacity</Text>
-        </TouchableOpacity>
+   return (
+      <View style={style.container}>
+         {/* <ScrollView  contentContainerStyle={{paddingBottom:10}}/> */}
+         <View style={style.mainbox}>
+            <Text style={style.subtext}>Expo React Native Class 01</Text>
+         </View>
+         <View>
+            <Text style={style.tophead}>Hi there 👋🏻</Text>
+            <Text style={style.minibox}>I am Shaharyar Ali</Text>
+            <Text style={style.minibox}>BSCS</Text>
+         </View>
+         <View >
+            {/*this button is not multiple customise things */}
+            <Button title="Submit" onPress={() => console.log("Shaharyar")} color={"blue"} />
+            <TouchableOpacity style={style.btnstyle}>
+               <Text style={style.plaintxt}>Touchable Opacity</Text>
+            </TouchableOpacity>
 
-        <Pressable style={({pressed})=>[
-          style.btnstyle,
-          pressed && style.btnhovestyle
-        ]}>
-          <Text style={style.plaintxt}>Pressable</Text>
-        </Pressable>
+            <Pressable style={({ pressed }) => [
+               style.btnstyle,
+               pressed && style.btnhovestyle
+            ]}>
+               <Text style={style.plaintxt}>Pressable</Text>
+            </Pressable>
 
-    </View>
-    </View>
+
+            <TextInput
+               style={style.input}
+               placeholder="Enter Name"
+               placeholderTextColor={"#888"}
+
+            />
+
+         </View>
+      </View>
    );
 
 }
 
 const style = StyleSheet.create({
-   container:{
-    flex: 1,
-    backgroundColor: "#fff"
+   container: {
+      flex: 1,
+      backgroundColor: "#fff"
    },
-   tophead:{
+   tophead: {
       fontWeight: "bold",
-    fontFamily: "Poppins",
-    fontSize: 30,
-    paddingTop: 10,
-    paddingLeft: 15,
+      fontFamily: "Poppins",
+      fontSize: 30,
+      paddingTop: 10,
+      paddingLeft: 15,
    },
-   mainbox:{
-    backgroundColor: "#1D2671",
-    width:"100%",
-    height: "20%",
-    borderBottomRightRadius: 20,
-    borderBottomLeftRadius: 20,
-    justifyContent: "center",
-    padding:20,
+   mainbox: {
+      backgroundColor: "#1D2671",
+      width: "100%",
+      height: "20%",
+      borderBottomRightRadius: 20,
+      borderBottomLeftRadius: 20,
+      justifyContent: "center",
+      padding: 20,
    },
-   minibox:{
+   minibox: {
       paddingTop: 15,
       paddingLeft: 15,
       borderColor: "#c4c4c4",
@@ -63,34 +72,41 @@ const style = StyleSheet.create({
       borderBlockColor: "#c4c4c4",
       borderWidth: 1,
       margin: 10,
-      borderRadius:10,
+      borderRadius: 10,
    },
-   subtext:{
-    color:  "#fff",
-    fontSize: 20,
-    textAlign: "center",
-    fontWeight: "bold",
-    fontFamily: "Poppins"
+   subtext: {
+      color: "#fff",
+      fontSize: 20,
+      textAlign: "center",
+      fontWeight: "bold",
+      fontFamily: "Poppins"
    },
-   btnstyle:{
-     margin:10,
-     padding: 10,
-     borderRadius: 10,
-     backgroundColor: "#000",
-     justifyContent: "center",
-     
+   btnstyle: {
+      margin: 10,
+      padding: 10,
+      borderRadius: 10,
+      backgroundColor: "#000",
+      justifyContent: "center",
+
    },
-   btnhovestyle:{
-     margin:10,
-     padding: 10,
-     borderRadius: 10,
-     backgroundColor: "#c4c4c4",
-     justifyContent: "center",
-     
+   btnhovestyle: {
+      margin: 10,
+      padding: 10,
+      borderRadius: 10,
+      backgroundColor: "#c4c4c4",
+      justifyContent: "center",
+
    },
-   plaintxt:{
-         color:  "#fff",
-         justifyContent: "center",
-         alignContent: "center",
+   plaintxt: {
+      color: "#fff",
+      justifyContent: "center",
+      alignContent: "center",
+   },
+   input: {
+      borderWidth: 1,
+      borderColor: "#888",
+      padding: 10,
+      fontSize: 16,
+      margin:10
    }
 });
