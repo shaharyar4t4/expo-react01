5214627d6b7f16ebfd2366ded95bb0eae650c2d5
Shaharyar <shaharyar4t4@gmail.com>
2025-07-19 01:25:50 +0500

    the counter is prefectly working

--- components/counter.tsx
+++ components/counter.tsx
@@ -6,12 +6,21 @@ const Counter = () => {
   // this hook which is used in react native
   const [counter, setCounter] = useState<number>(0);
   const [name, setName] = useState<String>("Shaharyar");
+  // this function which is show how to create a proper increament number
+  const increament =(): void =>{
+     setCounter(counter + 1);
+  }
+  // this function which is show how to create a proper decreament number
+  const decreament =(): void =>{
+     setCounter(counter - 1 );
+  }
+
   return (
     <View style = {style.continer}>
         <Text>Counter: {counter}</Text>
         <View style={style.btn}>
-        <Button title='Increament'/>
-        <Button title='Decreament' color="red"/>
+        <Button title='Increament'onPress={increament}/>
+        <Button title='Decreament' onPress={decreament} color="red"/>
         </View>
     </View>
   )
