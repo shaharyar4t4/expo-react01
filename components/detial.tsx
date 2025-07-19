66f7498b3b53474213845099f4b2ca784e1276f6
Shaharyar <shaharyar4t4@gmail.com>
2025-07-19 14:37:04 +0500

    make proper detial components

---
+++ components/detial.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { StyleSheet } from 'react-native'
+
+const detial = () => {
+  return (
+    <div>detial</div>
+  )
+}
+
+export default detial
+
+const style = StyleSheet.create({
+
+    
+});
