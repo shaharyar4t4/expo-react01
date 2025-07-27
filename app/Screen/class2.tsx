2e53edff8465814cfa161453e02b178dfd7bb648
Shaharyar <shaharyar4t4@gmail.com>
2025-07-27 16:12:04 +0500

    lean about saveareaview and status bar hiddern function

--- app/Screen/class2.tsx
+++ app/Screen/class2.tsx
@@ -1,18 +1,32 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StatusBar, StyleSheet, Text, View } from 'react-native'
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context'
 
 export const Class2 = () => {
   return (
-    <View style={style.container}>
-        <Text>Hello</Text>
+    // this particular is same for every mobile platform
+   // safeAreaProvider is used to handle the safe area insets on devices with notches or rounded corners
+   // SafeAreaView is used to render content within the safe area boundaries
+    <SafeAreaProvider>
+     <SafeAreaView style={style.container} edges={['top', 'left', 'right']}>
+      {/* StatusBar is used to control the appearance of the status bar */}
+      <StatusBar hidden={true}/>
+      <View >
+        <Text style={style.txt}>Hello</Text>
     </View>
+     </SafeAreaView>
+    </SafeAreaProvider>
+    
   )
 }
 
 const style = StyleSheet.create({
   container:{
     flex:0.2,
-    backgroundColor:"#000"
+    backgroundColor: "linear-gradient(90deg,rgba(42, 123, 155, 1) 0%, rgba(87, 199, 133, 1) 50%, rgba(237, 221, 83, 1) 100%)"
+  },
+  txt:{
+    color:"#fff"
   }
 
 })
