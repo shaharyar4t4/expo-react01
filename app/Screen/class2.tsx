479b3b580ead563734e8be7db0f1d40a5e2e6cc4
Shaharyar <shaharyar4t4@gmail.com>
2025-07-27 19:04:16 +0500

    updated

--- app/Screen/class2.tsx
+++ app/Screen/class2.tsx
@@ -7,25 +7,25 @@ export const Class2 = () => {
   return (
     // mainlayout is used to provide a consistent layout structure across the app this content for every screen 
     <MainLayout>
-        <View style={style.container}>
+      <View style={style.container}>
         <Text style={style.txt}>Hello</Text>
-    </View>
+      </View>
     </MainLayout>
-    
-    
-    
+
+
+
   )
 }
 const style = StyleSheet.create({
-  container:{
-    flex:0.2,
-    backgroundColor:  "radial-gradient(circle,rgba(42, 123, 155, 1) 0%, rgba(87, 199, 133, 1) 50%, rgba(237, 221, 83, 1) 100%)",
+  container: {
+    flex: 0.2,
+    backgroundColor: "radial-gradient(circle,rgba(42, 123, 155, 1) 0%, rgba(87, 199, 133, 1) 50%, rgba(237, 221, 83, 1) 100%)",
     borderBottomEndRadius: 30,
-    borderBottomStartRadius: 30, 
+    borderBottomStartRadius: 30,
     justifyContent: "center",
   },
-  txt:{
-    color:"#fff",
+  txt: {
+    color: "#fff",
     textAlign: "center",
   }
 
