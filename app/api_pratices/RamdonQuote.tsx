bfe6faad6ccce8a946f8978be357182ec014087c
Shaharyar <shaharyar4t4@gmail.com>
2025-08-19 17:12:11 +0500

    lean about how API is working with React Native and how to prefectly preform the roles in useEffect

---
+++ app/api_pratices/RamdonQuote.tsx
@@ -0,0 +1,75 @@
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+
+const RamdonQuote = () => {
+    const [count, setCount] = useState<number>(0);
+    const [countTwo, setCountTwo] = useState<number>(0);
+
+    // //this effect will run every time the component renders
+    // useEffect(() => {
+    //     console.log('Effect updated');
+    // });
+
+    // // This effect will run only once when the component mounts
+    useEffect(() => {
+        console.log('Effect Mounted');
+
+        // This function will run when the component unmounts
+        // or before the next effect runs
+
+        // in short ye function cleanup hota hai means koi screen ap chala jayao 
+        return () => {
+            console.log('Effect Unmounted');
+
+        }
+    }, []);
+    // this effect will run when the count state changes
+    // useEffect(() => {
+
+    //     console.log('Effect updated');
+    // }, [count]);
+
+
+    console.log('component rendered');
+    return (
+
+        <View style={styles.container}>
+            <Text style={
+                styles.txt
+            }>Effect Screen</Text>
+
+            <TouchableOpacity style={styles.btn}
+                onPress={() => {
+                    setCount(prev => prev + 1);
+                }}>
+                <Text style={styles.txt}>Count: {count}</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity style={styles.btn}
+                onPress={() => {
+                    setCountTwo(prev => prev + 1);
+                }}>
+                <Text style={styles.txt}>Count: {countTwo}</Text>
+            </TouchableOpacity>
+        </View>
+    )
+}
+
+export default RamdonQuote
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 20,
+        alignItems: 'center',
+    },
+    txt: {
+        fontSize: 20,
+    },
+    btn: {
+        backgroundColor: '#C4C4C4',
+        padding: 10,
+        borderRadius: 10,
+        marginTop: 20,
+    }
+})
