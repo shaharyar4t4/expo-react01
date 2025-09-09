13ba3b119f894bb120e65d90466d7f51b5c304f3
Shaharyar <shaharyar4t4@gmail.com>
2025-09-09 21:41:02 +0500

    updated

--- app/navigation/tabs/TabScreen.tsx
+++ app/navigation/tabs/TabScreen.tsx
@@ -1,10 +1,13 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import React from 'react';
 import Main from './tabscreen/Main';
+import Setting from './tabscreen/Setting';
+import Profile from './tabscreen/profile';
 
 export type RootTabParamList = {
   Main: undefined;
-//   About: undefined;
+  Setting: undefined;
+  Profile: undefined;
 //   Detialform: { id: string; name: string };
 //   Effect: undefined;
 //   RamdonQuote: undefined;
@@ -18,6 +21,9 @@ export const TabScreen = () => {
   return (
    <Tab.Navigator>
     <Tab.Screen  name={"Main"} component={Main} />
+    <Tab.Screen  name={"Setting"} component={Setting} />
+    <Tab.Screen  name={"Profile"} component={Profile} />
+
    </Tab.Navigator>
   )
 }
