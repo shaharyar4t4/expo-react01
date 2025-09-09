f1149d328702b7cadf38623bb83e06ded8ad40a1
Shaharyar <shaharyar4t4@gmail.com>
2025-09-09 22:58:33 +0500

    updated

--- app/navigation/tabs/TabScreen.tsx
+++ app/navigation/tabs/TabScreen.tsx
@@ -1,7 +1,9 @@
 import ProductDetial from '@/app/api_pratices/ProductDetial';
 import AntDesign from '@expo/vector-icons/AntDesign';
+import Entypo from '@expo/vector-icons/Entypo';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import React from 'react';
+
 
 import Main from './tabscreen/Main';
 import Setting from './tabscreen/Setting';
@@ -21,19 +23,41 @@ const Tab = createBottomTabNavigator<RootTabParamList>();
 
 export const TabScreen = () => {
     return (
-        <Tab.Navigator>
+        <Tab.Navigator
+            screenOptions={{
+                tabBarActiveTintColor: 'red',
+                tabBarInactiveTintColor: 'green',
+            }}
+        >
             <Tab.Screen name={"Main"} component={Main}
+
+
                 options={{
                     tabBarLabel: "Home",
-                    tabBarIcon: () => (
-                        <AntDesign name="home" size={24} color="black" />
-
+                    tabBarIcon: ({ size, color }) => (
+                        <AntDesign name="home" size={size} color={color} />
                     ),
                 }}
 
             />
-            <Tab.Screen name={"Setting"} component={Setting} />
-            <Tab.Screen name={"ProductDetial"} component={ProductDetial} />
+            <Tab.Screen name={"Setting"} component={Setting}
+                options={{
+                    tabBarLabel: "Setting",
+                    tabBarIcon: ({ color, size }) => (
+                        <AntDesign name="setting" size={size} color={color} />
+
+                    ),
+                }}
+            />
+            <Tab.Screen name={"ProductDetial"} component={ProductDetial}
+                options={{
+                    tabBarLabel: "Product Detial",
+                    tabBarIcon: ({ color, size }) => (
+                        <Entypo name="documents" size={size} color={color} />
+
+                    ),
+                }}
+            />
 
         </Tab.Navigator>
     )
