cc11ce3d56a7b1c2d25389d01b5ae11dcbc0043b
Shaharyar <shaharyar4t4@gmail.com>
2025-09-09 21:59:27 +0500

    tablable and tab Icon

--- app/navigation/tabs/TabScreen.tsx
+++ app/navigation/tabs/TabScreen.tsx
@@ -1,29 +1,40 @@
+import ProductDetial from '@/app/api_pratices/ProductDetial';
+import AntDesign from '@expo/vector-icons/AntDesign';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import React from 'react';
+
 import Main from './tabscreen/Main';
 import Setting from './tabscreen/Setting';
-import Profile from './tabscreen/profile';
 
 export type RootTabParamList = {
-  Main: undefined;
-  Setting: undefined;
-  Profile: undefined;
-//   Detialform: { id: string; name: string };
-//   Effect: undefined;
-//   RamdonQuote: undefined;
-//   ProductDetial: undefined;
-  
+    Main: undefined;
+    Setting: undefined;
+    ProductDetial: undefined;
+    //   Detialform: { id: string; name: string };
+    //   Effect: undefined;
+    //   RamdonQuote: undefined;
+    //   ProductDetial: undefined;
+
 };
 
 const Tab = createBottomTabNavigator<RootTabParamList>();
 
 export const TabScreen = () => {
-  return (
-   <Tab.Navigator>
-    <Tab.Screen  name={"Main"} component={Main} />
-    <Tab.Screen  name={"Setting"} component={Setting} />
-    <Tab.Screen  name={"Profile"} component={Profile} />
+    return (
+        <Tab.Navigator>
+            <Tab.Screen name={"Main"} component={Main}
+                options={{
+                    tabBarLabel: "Home",
+                    tabBarIcon: () => (
+                        <AntDesign name="home" size={24} color="black" />
 
-   </Tab.Navigator>
-  )
+                    ),
+                }}
+
+            />
+            <Tab.Screen name={"Setting"} component={Setting} />
+            <Tab.Screen name={"ProductDetial"} component={ProductDetial} />
+
+        </Tab.Navigator>
+    )
 }
